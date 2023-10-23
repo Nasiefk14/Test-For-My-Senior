7596557e9c57b87579234064e7295a674162873b
Nasief Khan <nasiefk14@gmail.com>
2023-10-23 13:00:55 +0200

    Updated Button Class

--- App For My Senior/src/components/Button.tsx
+++ App For My Senior/src/components/Button.tsx
@@ -1,18 +1,17 @@
 interface IButton {
   variation: string;
+  buttonText: string
 }
 
 const Button = (props: IButton) => {
-    const variation = props.variation;
-    const firstLetter = variation.charAt(0);
-    const firstLetterCap = firstLetter.toUpperCase()
-    const remainingLetters = variation.slice(1)
-    const updatedProp = firstLetterCap + remainingLetters
+  const variationType = props.variation;
+  const buttonText = props.buttonText;
   const handleClick = () => {
-    if (updatedProp === undefined) {
+    const validVariations = ["primary", "secondary", "warning", "danger"];
+    if (!validVariations.includes(variationType.toLowerCase())) {
       alert(`An Unknown Button Was Clicked!`);
     } else {
-      alert(`${updatedProp} Button Was Clicked!`);
+      alert(`${variationType} Button Was Clicked!`);
     }
   };
   return (
@@ -20,28 +19,18 @@ const Button = (props: IButton) => {
       <button
         onClick={handleClick}
         className={`${
-          props.variation.toLowerCase() === "primary"
+          variationType.toLowerCase() === "primary"
             ? "bg-[#0d6efd]"
-            : "bg-gray-300"
-        }
-        ${
-          props.variation.toLowerCase() === "secondary"
+            : variationType.toLowerCase() === "secondary"
             ? "bg-[#6c757d]"
-            : "bg-gray-300"
-        }
-        ${
-          props.variation.toLowerCase() === "warning"
+            : variationType.toLowerCase() === "warning"
             ? "bg-[#ffc107]"
-            : "bg-gray-300"
-        }
-        ${
-          props.variation.toLowerCase() === "danger"
+            : variationType.toLowerCase() === "danger"
             ? "bg-[#dc3545]"
             : "bg-gray-300"
-        }
-         bg-blue-300 w-[150px] h-[50px] border-2 border-gray-950 text-2xl m-2 font-bold rounded-lg `}
+        } w-[150px] h-[50px] border-2 border-gray-950 text-2xl m-2 font-bold rounded-lg `}
       >
-        Our Button!
+        {buttonText}
       </button>
     </>
   );
--- App For My Senior/src/pages/ButtonShowcase.tsx
+++ App For My Senior/src/pages/ButtonShowcase.tsx
@@ -2,12 +2,12 @@ import Button from "../components/Button"
 
 const ButtonShowcase = () => {
   return (
-    <div className="w-full h-screen pt-[100px] p-4">
-        <h1 className="text-center text-3xl font-bold">Here is where you can show buttons using there variation prop</h1>
-        <Button variation={"primary"} />
-        <Button variation={"secondary"} />
-        <Button variation={"Warning"} />
-        <Button variation={"danger"} />
+    <div className="w-[60%] h-screen m-auto pt-[100px] text-center">
+        <h1 className="text-center text-3xl font-bold mb-5">Here is where you can show buttons using there variation prop</h1>
+        <Button variation={"primary"} buttonText={"Primary"}/>
+        <Button variation={"secondssdsefvary"} buttonText={"Broken"}/>
+        <Button variation={"Warning"} buttonText={"Warning"}  />
+        <Button variation={"danger"} buttonText={"Danger"} />
     </div>
   )
 }
