f0ad2fa0230c0980267c7cea54586a4a6a4df111
Nasief Khan <nasiefk14@gmail.com>
2023-10-24 00:06:45 +0200

    Added hover effect & code refactor

--- App For My Senior/src/pages/ButtonShowcase.tsx
+++ App For My Senior/src/pages/ButtonShowcase.tsx
@@ -5,7 +5,7 @@ const ButtonShowcase = () => {
     <div className="w-[60%] h-screen m-auto pt-[100px] text-center">
         <h1 className="text-center text-3xl font-bold mb-5">Here is where you can show buttons using there variation prop</h1>
         <Button variation={"primary"} buttonText={"Primary"}/>
-        <Button variation={"secondssdsefvary"} buttonText={"Broken"}/>
+        <Button buttonText={"Test"}/>
         <Button variation={"Warning"} buttonText={"Warning"}  />
         <Button variation={"danger"} buttonText={"Danger"} />
     </div>
