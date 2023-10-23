719afa0b70de65259c989081d908ca67bb2257da
Nasief Khan <nasiefk14@gmail.com>
2023-10-23 14:02:07 +0200

    Removed React Router

--- App For My Senior/src/App.tsx
+++ App For My Senior/src/App.tsx
@@ -1,15 +1,12 @@
 import Navbar from "./components/Navbar";
 import ButtonShowcase from "./pages/ButtonShowcase";
 
-import {Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
     <>
     <Navbar />
-    <Routes>
-    <Route path="/task-one" element={<ButtonShowcase />}/>
-    </Routes>
+    <ButtonShowcase />
     </>
   );
 }
