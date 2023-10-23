0a83784f5f83220824d9c3789781d7eb7d7db727
Nasief Khan <nasiefk14@gmail.com>
2023-10-24 00:06:52 +0200

    Created interface

--- App For My Senior/src/components/Button.tsx
+++ App For My Senior/src/components/Button.tsx
@@ -1,38 +1,34 @@
-interface IButton {
-  variation: string;
-  buttonText: string
-}
+import { IButton } from "../interfaces/interfaces";
 
 const Button = (props: IButton) => {
-  const variationType = props.variation;
+  const validVariations = ["primary", "secondary", "warning", "danger"];
+  const variationType = props.variation || "primary";
   const buttonText = props.buttonText;
+
   const handleClick = () => {
-    const validVariations = ["primary", "secondary", "warning", "danger"];
     if (!validVariations.includes(variationType.toLowerCase())) {
       alert(`An Unknown Button Was Clicked!`);
     } else {
       alert(`${variationType} Button Was Clicked!`);
     }
   };
+
+  const buttonClass = `${
+    variationType.toLowerCase() === "primary"
+      ? "bg-[#0d6efd] bg-opacity-80  hover:bg-opacity-100 hover:text-white text-black"
+      : variationType.toLowerCase() === "secondary"
+      ? "bg-[#6c757d] bg-opacity-80  hover:bg-opacity-100 hover:text-white text-black"
+      : variationType.toLowerCase() === "warning"
+      ? "bg-[#ffc107] bg-opacity-80  hover:bg-opacity-100 hover:text-white text-black"
+      : variationType.toLowerCase() === "danger"
+      ? "bg-[#dc3545] bg-opacity-80  hover:bg-opacity-100 hover:text-white text-black"
+      : "bg-gray-300"
+  } w-[150px] h-[50px] border-2 border-gray-950 text-2xl m-2 font-bold rounded-lg text-white`;
+
   return (
-    <>
-      <button
-        onClick={handleClick}
-        className={`${
-          variationType.toLowerCase() === "primary"
-            ? "bg-[#0d6efd]"
-            : variationType.toLowerCase() === "secondary"
-            ? "bg-[#6c757d]"
-            : variationType.toLowerCase() === "warning"
-            ? "bg-[#ffc107]"
-            : variationType.toLowerCase() === "danger"
-            ? "bg-[#dc3545]"
-            : "bg-gray-300"
-        } w-[150px] h-[50px] border-2 border-gray-950 text-2xl m-2 font-bold rounded-lg `}
-      >
-        {buttonText}
-      </button>
-    </>
+    <button onClick={handleClick} className={buttonClass}>
+      {buttonText}
+    </button>
   );
 };
 
---
+++ App For My Senior/src/interfaces/interfaces.ts
@@ -0,0 +1,4 @@
+export interface IButton {
+    variation?: string;
+    buttonText: string;
+  }
