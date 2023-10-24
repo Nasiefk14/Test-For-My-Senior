214de411cfb067d1a35eee25dea20a7641f66dc1
Nasief Khan <nasiefk14@gmail.com>
2023-10-24 11:12:15 +0200

    Added interface for nav tabs

--- App For My Senior/src/interfaces/interfaces.ts
+++ App For My Senior/src/interfaces/interfaces.ts
@@ -1,4 +1,10 @@
 export interface IButton {
     variation?: string;
     buttonText: string;
+  };
+
+export interface ITabPanelProps {
+    children?: React.ReactNode;
+    index: number;
+    value: number;
   }
