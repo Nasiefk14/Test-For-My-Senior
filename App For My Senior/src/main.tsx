2dc112f5a3446c7226e5ca080c919d3542e4c423
Nasief Khan <nasief.khan@wyetalk.com>
2023-10-27 17:23:55 +0200

    Removed Easy Peasy

--- App For My Senior/src/main.tsx
+++ App For My Senior/src/main.tsx
@@ -1,10 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { StoreProvider } from 'easy-peasy';
-import store from './store';
-import { useStoreState } from './store/hooks';
-import 
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
--- App For My Senior/src/store/index.ts
+++
@@ -1,7 +0,0 @@
-import { createStore, persist } from "easy-peasy";
-import { StoreModel } from "./interface";
-import todo from "./model";
-
-const store = createStore<StoreModel>(persist(todo));
-
-export default store;
