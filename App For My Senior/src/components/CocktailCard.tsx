354b9862b50158b313bfdeaa3d483ad4b4fa3271
Nasief Khan <nasief.khan@wyzetalk.com>
2023-10-30 16:01:37 +0200

    Finished up task 3

--- App For My Senior/src/components/CocktailCard.tsx
+++ App For My Senior/src/components/CocktailCard.tsx
@@ -1,12 +1,21 @@
+import { useState } from "react";
 import { ICocktail } from "../types/interfaces/interfaces";
 
 const CocktailCard = (props: ICocktail) => {
-  return (
-    <div key={props.idDrink}>
-      <h2>{props.strDrink}</h2>
-      <img src={props.strDrinkThumb} alt={props.strDrink} />
-    </div>
-  );
+    const [showing, setShowing] = useState(false); 
+    return (
+        <div key={props.idDrink} className="text-center bg-gray-100 rounded-md border-4 relative w-60 h-72" style={{border: showing ? '4px solid #FFD700' : 'none'}} onClick={() => setShowing(!showing)}>
+            <div style={{display: showing ? 'block' : 'none'}} className="absolute top-0 right-0 w-8 h-8 text-[#FFD700]">
+                &#9733;
+            </div>                
+            <p className="pt-4 text-md">{props.strDrink}</p>
+            <img
+                src={props.strDrinkThumb}
+                alt={props.strDrink}
+                className="pt-2 pb-2 w-[80%] h-52 absolute bottom-0 left-1/2 transform -translate-x-1/2"
+            />
+        </div>
+    );
 };
 
 export default CocktailCard;
--- App For My Senior/src/pages/CocktailListShowcase.tsx
+++ App For My Senior/src/pages/CocktailListShowcase.tsx
@@ -3,27 +3,29 @@ import { ICocktail } from "../types/interfaces/interfaces";
 import CocktailCard from "../components/CocktailCard";
 
 const CocktailListShowcase = () => {
-    const [cocktails, setCocktails] = useState([]);
+  const [cocktails, setCocktails] = useState([]);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const response = await fetch(
-                "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic"
-            );
-            const data = await response.json();
-            setCocktails(data.drinks.slice(0, 10))
-        };
-        fetchData();
-    }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic"
+      );
+      const data = await response.json();
+      setCocktails(data.drinks.slice(0, 10));
+    };
+    fetchData();
+  }, []);
 
-    return (
-        <div>
-            <h1>My Cocktail List</h1>
-            {cocktails.map((cocktail: ICocktail) => (
-                <CocktailCard {...cocktail} />
-            ))}
-        </div>
-    );
+  return (
+    <div className="h-screen m-auto text-center">
+      <h1 className="text-center text-3xl font-bold mb-5">My Cocktail List</h1>
+      <div className="grid grid-cols-5 gap-4">
+        {cocktails.map((cocktail: ICocktail) => (
+          <CocktailCard {...cocktail} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default CocktailListShowcase;
